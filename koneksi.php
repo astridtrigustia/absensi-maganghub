c58a9c92bda083ed28a504dab8a851603e289012
astridtrigustia <115436916+astridtrigustia@users.noreply.github.com>
2026-01-17 10:19:10 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,12 +1,15 @@
 <?php
 $conn = mysqli_connect(
-    "sql206.infinityfree.com",
-    "ifo_40922998",
-    "xK098RhBh2",
-    "ifo_40922998_db_absensi"
+    $_ENV['MYSQL_HOST'],
+    $_ENV['MYSQL_USER'],
+    $_ENV['MYSQL_PASSWORD'],
+    $_ENV['MYSQL_DATABASE'],
+    $_ENV['MYSQL_PORT']
 );
 
 if (!$conn) {
     die("Koneksi database gagal: " . mysqli_connect_error());
 }
 ?>
+
+
