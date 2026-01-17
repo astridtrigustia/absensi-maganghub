d72c31f9df41a91a885a702c3d31053a9dfbb95f
astridtrigustia <115436916+astridtrigustia@users.noreply.github.com>
2026-01-17 10:56:05 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,15 +1,13 @@
 <?php
 $conn = mysqli_connect(
-    $_ENV['MYSQL_HOST'],
-    $_ENV['MYSQL_USER'],
-    $_ENV['MYSQL_PASSWORD'],
-    $_ENV['MYSQL_DATABASE'],
-    $_ENV['MYSQL_PORT']
+    $_ENV['MYSQLHOST'],
+    $_ENV['MYSQLUSER'],
+    $_ENV['MYSQLPASSWORD'],
+    $_ENV['MYSQLDATABASE'],
+    $_ENV['MYSQLPORT']
 );
 
 if (!$conn) {
     die("Koneksi database gagal: " . mysqli_connect_error());
 }
 ?>
-
-
